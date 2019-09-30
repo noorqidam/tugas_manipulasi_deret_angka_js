1e0631047d51ab3ab0f45f32bb5822de63cfb831
noorqidam <noorqidam@gmail.com>
2019-09-30 10:22:45 +0700

    Tugas Manipulasi Deret Angka JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas_manipulasi_deret_angka.js
@@ -0,0 +1,22 @@
+var str = 'Hallo';
+console.log(str.split('').reverse().join(''));
+
+function deretAngka(n) {
+    var hasil = ''
+    for (var i = 1; i <= n; i++) {
+        if ((i % 3 === 0) && (i % 5 === 0)) {
+            hasil += 'NIOMIC '
+        } else if (i % 3 === 0) {
+            hasil += 'NIO '
+        } else if (i % 5 === 0) {
+            hasil += 'MIC '
+        } else {
+            hasil += i + ' '
+        }
+    }
+    return hasil
+}
+
+console.log(deretAngka(10));
+console.log(deretAngka(20));
+console.log(deretAngka(30));
